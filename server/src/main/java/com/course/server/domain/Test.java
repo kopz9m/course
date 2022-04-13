4355224ef6781dce3b53009629f1fa3d641cde8d
v_wyzhangpengcheng <patrzhang@deloitte.com.cn>
2022-04-13 15:23:39 +0800

    mybatis genatator

--- server/src/main/java/com/course/server/domain/Test.java
+++ server/src/main/java/com/course/server/domain/Test.java
@@ -1,13 +1,15 @@
 package com.course.server.domain;
 
 public class Test {
-    private String id;
+    private Integer id;
+
     private String name;
-    public String getId() {
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -19,5 +21,15 @@ public class Test {
         this.name = name;
     }
 
-
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", name=").append(name);
+        sb.append("]");
+        return sb.toString();
+    }
 }
